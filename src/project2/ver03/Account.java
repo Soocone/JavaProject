e305eaa3615dfbc9806e9449477a45143fa6ff77
Soocone <wltn3185@naver.com>
2021-10-27 11:30:47 +0900

    ver3 commit

---
+++ src/project2/ver03/Account.java
@@ -0,0 +1,27 @@
+package project2.ver03;
+
+public abstract class Account
+{
+	public String accountID; //계좌번호
+	public String customName; //고객이름
+	public int accMoney; //잔고
+	
+	
+	public Account(String accountID, String customName, int accMoney)
+	{
+		this.accountID = accountID;
+		this.customName = customName;
+		this.accMoney = accMoney;
+	}
+	
+	public void showAccInfo() {
+		System.out.println("┏━━━━━━━━━━━━━━━━━━━━━━━━━┓");
+		System.out.println(" 계좌번호: "+ accountID);
+		System.out.println(" 예금주: "+ customName);
+		System.out.println(" 잔고: "+ accMoney);
+	}
+	
+	public void deposit(int deposit) {
+	}
+	
+}
